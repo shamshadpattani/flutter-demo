52114f5de599889ecaea35ba0b317e4405543772
shamshad <muhammedshamshadp@gmail.com>
2023-07-26 18:50:42 +0530

    fix tap

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:demo_flutter/pages/home.dart';
+
 import 'package:flutter/material.dart';
+import 'package:untitled/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,14 +8,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: const HomePage(),
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins') ,
+      home:  const HomePage()
+
+
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -6,8 +7,29 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-      ),
-    )
+        title:Text('Break fast',
+        style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold,)),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        leading: GestureDetector(
+           onTap: (){},
+            child: Container(
+          decoration:BoxDecoration(color: Colors.white60,borderRadius: BorderRadius.circular(4)),
+          margin:const EdgeInsets.all(4),
+          alignment: Alignment.center,
+          child: SvgPicture.asset("assets/icons/arrow_left.svg",width: 20,height:20,))
+          ),
+        actions: [
+          GestureDetector(
+          onTap: (){},
+         child: Container(
+            decoration:BoxDecoration(color: Colors.white60,borderRadius: BorderRadius.circular(4)),
+            margin:const EdgeInsets.all(4),
+            alignment: Alignment.center,
+            width: 40,
+            child: SvgPicture.asset("assets/icons/dot-menu.svg",width: 20,height:20,))],
+       ),
+    );
   }
 }
